f741cf21fb182e60f5e3cc10d2f2e0519ba5a6ba
Lincoln Rogala Moro <lincoln.moro.bc@gmail.com>
2022-02-20 20:02:48 -0300

    Update index.php
    Added second question code

--- index.php
+++ index.php
@@ -40,4 +40,73 @@ if(isset($_POST['questao1'])) {
 
     echo "</div>";
 }
+?>
 
+<form method="post" action="">
+    <div>
+        <label for="anagram">Digite o valor aqui</label>
+        <input type="text" id="password" name="password">
+    </div>
+    
+    <div>
+        <input type="submit" name="questao2" value="Enviar">
+    </div>
+</form>
+
+<?php
+
+if(isset($_POST['questao2'])) {
+    $password = $_POST['password'];
+
+    echo "<div class='box'><h2>Resultado</h2>";
+
+    if(
+        preg_match('/[!@#$%\^&*()-\+]/', $password) &&
+        preg_match('/[a-z0-9]/', $password) && 
+        preg_match('/[A-Z]/', $password) &&
+        strlen($password) > 6) {
+            echo "A senha é segura :D";
+        }
+    else {
+        echo "A senha deve conter";
+    }
+}
+?>
+
+    
+
+<form method="post" action="">
+    <div>
+        <label for="anagram">Digite o valor aqui</label>
+        <input type="text" id="anagram" name="anagram">
+    </div>
+    
+    <div>
+        <input type="submit" name="questao3" value="Enviar">
+    </div>
+</form>
+
+<?php
+
+if(isset($_POST['questao3'])) {
+    $string = $_POST['anagram'];
+    $anagram = str_split($string);
+    $count = 0;
+
+    echo "<div class='box'><h2>Resultado</h2>";
+
+   for($i = 0; $i < $anagram; $i++) {
+       for($j = 1; $j <= $anagram; $j++) {
+           var_dump($anagram);
+           if($anagram[$i] == $anagram[$j]) {
+                $count == $count++;
+                if($i + 1 > $j) { 
+                    $count == $count++;
+                }
+           }
+       }
+   }
+    echo $count;
+    echo "</div>";
+}
+?>
